8c4db270660a135d89be5512bb4a55eb4631b2c3
Moliksi <104253029+Moliksi@users.noreply.github.com>
2025-03-24 18:26:16 +0400

    Add files via upload

---
+++ Damian Gonzalez/dg_simpleCamFollow/Scripts/dg_simpleCamFollow.cs
@@ -0,0 +1,40 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+using UnityEngine;
+
+public class dg_simpleCamFollow : MonoBehaviour
+{
+    public Transform target;
+    [Range(1f,40f)] public float laziness = 10f;
+    public bool lookAtTarget = true;
+    public bool takeOffsetFromInitialPos = true;
+    public Vector3 generalOffset;
+    Vector3 whereCameraShouldBe;
+    bool warningAlreadyShown = false;
+    [SerializeField]
+    float leftlimit;
+    [SerializeField]
+    float rigthlimit;
+    [SerializeField]
+    float uplimit;
+    [SerializeField]
+    float downlimit;
+    private void Start() {
+        if (takeOffsetFromInitialPos && target != null) generalOffset = transform.position - target.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (target != null) {
+            whereCameraShouldBe = target.position + generalOffset;
+            transform.position = Vector3.Lerp(transform.position, whereCameraShouldBe, 1 / laziness);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, leftlimit, rigthlimit), Mathf.Clamp(transform.position.y, downlimit, uplimit), transform.position.z);
+            if (lookAtTarget) transform.LookAt(target);
+        } else {
+            if (!warningAlreadyShown) {
+                Debug.Log("Warning: You should specify a target in the simpleCamFollow script.", gameObject);
+                warningAlreadyShown = true;
+            }
+        }
+    }
+}
